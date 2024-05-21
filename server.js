c80d39fc0407e58184cdb682cfe07e5d51e0e7b5
yoga <yogaadi0902@gmail.com>
2024-05-21 13:55:39 +0700

    Testing 0.2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", async (req, res) => {
     res.render("index.ejs", { posts: response.data });
   } catch (error) {
     console.error("Error fetching posts:", error);
-    res.status(500).json({ message: "Error fetching posts" });
+    res.status(500).json({ message: `Error fetching posts`+error });
   }
 });
 
